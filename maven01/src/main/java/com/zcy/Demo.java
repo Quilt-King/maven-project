57f8da7faa5dbb1602a429f746bc75f903885f13
zcy <208114298@qq.com>
2022-02-13 17:50:07 +0800

    second commit

--- maven01/src/main/java/com/zcy/Demo.java
+++ maven01/src/main/java/com/zcy/Demo.java
@@ -3,6 +3,7 @@ package com.zcy;
 public class Demo {
     public String say(String name){
         System.out.println("hello "+name);
+        System.out.println("hello lq");
         return "hello "+name;
 
     }
