0a0532f5914a11e1e5f62ffa2ba2d4aa27164742
zcy <208114298@qq.com>
2022-02-13 17:58:29 +0800

    hot-fix commit

--- maven01/src/main/java/com/zcy/Demo.java
+++ maven01/src/main/java/com/zcy/Demo.java
@@ -4,6 +4,7 @@ public class Demo {
     public String say(String name){
         System.out.println("hello "+name);
         System.out.println("hello lq");
+        System.out.println("hello zhuzhu");
         return "hello "+name;
 
     }
